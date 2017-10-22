a5efef9f11710b0d738d51d40839d07ae0b4c3d0
OuSs <oussama.almalawi@hotmail.com>
2017-10-22 08:38:53 +0300

    ProgFields

--- system/Send_email.php
+++ system/Send_email.php
@@ -2,9 +2,9 @@
 
     $con = new mysqli('localhost', 'root','' , 'db_progfields_8003112356_v');
 
-    if ($con->connect_error) {
-        die("Connection failed: " . $con->connect_error);
-    }
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+}
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
@@ -14,37 +14,58 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $subject_DB = $_POST['subject'];
     $message_DB = $_POST['message'];
 
-    /* change character set to utf8 */
-    $con->set_charset("utf8");
+    if (empty($Fname) || empty($Lname) || empty($email) || empty($subject_DB) || empty($message_DB)){
+        echo 'خطأ, الرجاء تعبئة كامل الحقول';
+        echo 'Error, please fill out the entire fields';
+        echo '<meta http-equiv="Refresh" content="3; \'../index.php?pid=' . $_GET['pid'] . '\'">';
+    }else {
 
-    $sql = "INSERT INTO contact_us (First_Name,Last_Name,Email,Subject,Message)
+
+        /* change character set to utf8 */
+        $con->set_charset("utf8");
+
+        $sql = "INSERT INTO contact_us (First_Name,Last_Name,Email,Subject,Message)
                             VALUES ('$Fname','$Lname','$email','$subject_DB','$message_DB')";
 
-    if (mysqli_query($con, $sql)) {
-        //####################################################################################################################
+        if (mysqli_query($con, $sql)) {
+            //####################################################################################################################
 
-        //Send Email
+//        Send Email
 
-        //    $to = 'progFields@gmail.com';
-        //    $subject = 'From : Contact us Subject' . $_POST['subject'];
-        //    $message = $_POST['message'];
+            $to = 'info@progfields.com';
+            $subject = $_POST['subject'];
+            $message = '
+                        <html>
+                            <head>
+                              <title>Birthday Reminders for August</title>
+                            </head>
+                            <body>
+                                  <p>From : ' . $_POST['Fname'] . $_POST['Lname'] . '</p>
+                                  <p>Email : ' . $_POST['email'] . '</p>
+                                  <br>
+                                  <br>
+                                  <p> ' . $_POST['message'] . '</p>
+                            </body>
+                        </html>
+                        ';
 
-                // To send HTML mail, the Content-type header must be set
-        //    $headers  = 'MIME-Version: 1.0' . "\r\n";
-        //    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
 
-        // Additional headers
-        //    $headers .= 'To: '.$email."\r\n";
-        //    $headers .= 'From: Programing Fields <ProgFields@gmail.com>' . "\r\n";
-        //    $headers .= 'Cc: birthdayarchive@example.com' . "\r\n";
-        //    $headers .= 'Bcc: birthdaycheck@example.com' . "\r\n";
+//         To send HTML mail, the Content-type header must be set
+            $headers = 'MIME-Version: 1.0' . "\r\n";
+            $headers .= 'Content-type: text/html; charset=utf8' . "\r\n";
 
-        //    mail($to, $subject, $message, $headers);
+//         Additional headers
+//            $headers .= 'To: ' . '<progfields@gmail.com>' . "\r\n";
+            $headers .= 'From: ' . $email . "\r\n";
+            $headers .= 'Cc: ' . '<progfields@gmail.com>' . "\r\n";
+//            $headers .= 'Bcc: birthdaycheck@example.com' . "\r\n";
 
-        echo '<meta http-equiv="Refresh" content="0; \'../index.php?pid='.$_GET['pid'].'\'">';
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($con);
+            mail($to, $subject, $message, $headers);
+
+            echo '<meta http-equiv="Refresh" content="0; \'../index.php?pid=' . $_GET['pid'] . '\'">';
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($con);
+        }
     }
-
 }
 ?>
